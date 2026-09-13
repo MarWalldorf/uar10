26f8ed353e3a87b83432ddfbf76a06073e2c5f41
Martin1337 <115101060+MarWalldorf@users.noreply.github.com>
2026-09-13 15:40:24 +0200

    Add files via upload

---
+++ UAR_ARX/Testy.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace TESTY_ModelARX {
+void wykonaj_testy();
+}
+
+namespace TESTY_RegulatorPID {
+void wykonaj_testy();
+}
+
+namespace TESTY_ProstyUAR {
+void wykonaj_testy();
+}
